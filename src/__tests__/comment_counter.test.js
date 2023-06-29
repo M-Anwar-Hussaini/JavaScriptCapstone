642e0c7ca663c811387aa8428f251ceca185f622
Sergio Andres Usma <126805818+sergio-usma@users.noreply.github.com>
2023-06-29 11:01:13 -0500

    Added additional test for comment counter using describe

--- src/__tests__/comment_counter.test.js
+++ src/__tests__/comment_counter.test.js
@@ -1,8 +1,17 @@
 import PopUp from '../modules/popUp.js';
 
-test('The number of comments turkey should be 1', async () => {
-  const country = 'turkey';
-  const expected = 1;
-  const result = PopUp.getComments(country);
-  expect(result).toBe(expected);
+describe('countComments', () => {
+  test('The number of comments turkey should be 1', async () => {
+    const country = 'turkey';
+    const expected = 1;
+    const result = PopUp.getComments(country);
+    expect(result).toBe(expected);
+  });
+
+  test('The number of comments for colombia should be 24', async () => {
+    const country = 'colombia';
+    const expected = 6;
+    const result = PopUp.getComments(country);
+    expect(result).toBe(expected);
+  });
 });
