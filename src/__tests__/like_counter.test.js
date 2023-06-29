359f6d800843164a707a4a70537ecfd6e8a02793
Anwar <anwarhussaini160@gmail.com>
2023-06-29 09:54:39 +0430

    Test the number of likes

--- src/__tests__/like_counter.test.js
+++ src/__tests__/like_counter.test.js
@@ -1,12 +1,18 @@
 import Countries from '../modules/rest_countries.js';
 import DOM from '../modules/DOM.js';
 
-const countries = new Countries();
-
 describe('Test the counter functions', () => {
   test('The amount all items should be 24', () => {
-    // Assert
+    // Arrange
+    const countries = new Countries();
+    // Act
     const counts = countries.getSelectedCountries();
+    // Assert
     expect(counts).toHaveLength(24);
+  });
+
+  // Checks the number of likes to be greater or equal to be 0
+  test('The number of all likes should be greater than or equal to 0', () => {
+    expect(DOM.getLikes()).toBeGreaterThanOrEqual(0);
   });
 });
